93d80bcbbb861650476d6873627bb0a2f5a89e4e
Andy Howell <andy22b@users.noreply.github.com>
2022-10-03 16:55:31 +1300

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const exec = (cmd, args=[]) => new Promise((resolve, reject) => {
 });
 
 const main = async () => {
-    await exec('bash', [path.join(__dirname, './main.sh')]);
+    await exec('bash -l {0} ', [path.join(__dirname, './main.sh')]);
 };
 
 main().catch(err => {
